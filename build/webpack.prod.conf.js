9fabaac5f1951ba632b90dba503bb29bec6370e0
qiu.bl <qbl378739311@163.com>
2019-04-24 15:32:45 +0800

    修改bugger
    1. 通过打包 调用文件的形式，解决IE 不兼容的问题
    main.js 引入：
    import Album from 'vue-pop-album'
    import 'vue-pop-album/dist/vue-pop-album.css'

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -50,7 +50,7 @@ const webpackConfig = merge(baseWebpackConfig, {
     }),
     // extract css into its own file
     new ExtractTextPlugin({
-      filename: utils.assetsPath('css/[name].[contenthash].css'),
+      filename: '../dist/vue-pop-album.css',
       // Setting the following option to `false` will not extract CSS from codesplit chunks.
       // Their CSS will instead be inserted dynamically with style-loader when the codesplit chunk has been loaded by webpack.
       // It's currently set to `true` because we are seeing that sourcemaps are included in the codesplit bundle as well when it's `false`,
